59d368e9d107f5c1fc8a8fa388dc5627eb0cd866
Agustin ORTU <ortu.agustin@gmail.com>
2018-09-26 18:40:20 -0300

    app brillo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Brightness } from '@ionic-native/brightness';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
     ContactPage,
-    HomePage,
-    TabsPage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -26,14 +23,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    ContactPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Brightness,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the ContactPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
   selector: 'page-contact',
-  templateUrl: 'contact.html'
+  templateUrl: 'contact.html',
 })
 export class ContactPage {
+  email: string = '';
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.email = this.navParams.get('email');
   }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ContactPage');
+  }
 }
